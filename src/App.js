586dfd1dd9cf999bcec9301920e1a161f5c9e931
Ben <benedikt.mehler@mailbox.org>
2023-02-06 15:51:50 +0100

    create app Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
+import styled from "@emotion/styled";
+import InputNote from "./components/InputNote";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Navbar>
+        <NavText>Notes!</NavText>
+      </Navbar>
+      <main>
+        <InputNote />
+      </main>
+    </>
   );
 }
 
 export default App;
+
+const Navbar = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  background-color: lightblue;
+  height: 2.5rem;
+  width: 100%;
+  margin-bottom: 1rem;
+`;
+
+const NavText = styled.h2`
+  margin: 0;
+  font-size: x-large;
+  text-decoration: underline;
+`;
